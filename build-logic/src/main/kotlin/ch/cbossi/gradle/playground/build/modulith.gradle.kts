d90b75ae40527409fd8e872fc7b511e0e21a8933
Christian Bosshard <bosshardchr@gmail.com>
2024-12-25 07:17:04 +0100

    Move test dependencies to basic gradle plugin.

--- build-logic/src/main/kotlin/ch/cbossi/gradle/playground/build/modulith.gradle.kts
+++ build-logic/src/main/kotlin/ch/cbossi/gradle/playground/build/modulith.gradle.kts
@@ -3,3 +3,9 @@ import org.gradle.kotlin.dsl.kotlin
 plugins {
     kotlin("jvm")
 }
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
+    testImplementation("org.assertj:assertj-core:3.27.0")
+}
--- webshop/customer/api/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
-    testImplementation("org.assertj:assertj-core:3.27.0")
-}
