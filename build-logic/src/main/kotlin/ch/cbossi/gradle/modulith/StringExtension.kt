1de7bbadb2d4cc0a524fe1d86519334235adcf46
Christian Bosshard <bosshardchr@gmail.com>
2024-12-28 19:21:25 +0100

    Make function internal.

--- build-logic/src/main/kotlin/ch/cbossi/gradle/modulith/StringExtension.kt
+++ build-logic/src/main/kotlin/ch/cbossi/gradle/modulith/StringExtension.kt
@@ -1,3 +1,3 @@
 package ch.cbossi.gradle.modulith
 
-fun String?.emptyToNull() = this?.let { if (isEmpty()) null else this }
+internal fun String?.emptyToNull() = this?.let { if (isEmpty()) null else this }
