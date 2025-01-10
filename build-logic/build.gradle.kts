d968bcbd1126c5d694ef4a07dc8b8a617ebdbfb1
Christian Bosshard <bosshardchr@gmail.com>
2025-01-10 22:25:45 +0100

    Fix comment.

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    // since Kotlin plugin is applied from within the ArciphantCorePlugin, we need this dependency here
+    // required since Kotlin plugin is applied from within the component-plugin
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
