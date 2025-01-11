b2cea3cc71945fb8567e054bbf697da7d696ddaa
Christian Bosshard <bosshardchr@gmail.com>
2025-01-11 17:28:48 +0100

    Remove unnecessary dependency.

--- arciphant-gradle-plugin/build.gradle.kts
+++ arciphant-gradle-plugin/build.gradle.kts
@@ -8,9 +8,6 @@ plugins {
 }
 
 dependencies {
-    // since Kotlin plugin is applied from within the ArciphantCorePlugin, we need this dependency here
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
     testImplementation("org.assertj:assertj-core:3.27.0")
