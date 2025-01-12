45b7b6e5014888550c5b45387a5a78b78f721923
Christian Bosshard <bosshardchr@gmail.com>
2025-01-12 21:16:43 +0100

    Implement integration test.

--- arciphant-sample-project/build-logic/src/main/kotlin/component.gradle.kts
+++ arciphant-sample-project/build-logic/src/main/kotlin/component.gradle.kts
@@ -12,6 +12,9 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
     testImplementation("org.assertj:assertj-core:3.27.0")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // webflux is required to use WebTestClient
+    testRuntimeOnly("org.springframework.boot:spring-boot-starter-webflux")
 }
 
 tasks.withType<Test> {
---
+++ arciphant-sample-project/src/test/kotlin/ch/ergon/arciphant/sample/CustomerIntegrationTest.kt
@@ -0,0 +1,32 @@
+package ch.ergon.arciphant.sample
+
+import ch.ergon.arciphant.sample.customer.webapi.CustomerDto
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class CustomerIntegrationTest {
+
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun `it should get customers`() {
+        val customers = webTestClient.get()
+            .uri("customers")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CustomerDto::class.java)
+            .returnResult()
+            .responseBody
+
+        assertThat(customers).containsExactlyInAnyOrder(
+            CustomerDto("Max", "Muster"),
+            CustomerDto("John", "Doe"),
+        )
+    }
+}
