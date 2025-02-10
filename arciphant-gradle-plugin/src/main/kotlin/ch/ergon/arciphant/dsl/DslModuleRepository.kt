7f4f192f68cbb2ac308d1ebd79a1add754da3cf8
Christian Bosshard <bosshardchr@gmail.com>
2025-02-10 17:46:02 +0100

    Improve method ordering.

--- arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/dsl/DslModuleRepository.kt
+++ arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/dsl/DslModuleRepository.kt
@@ -6,14 +6,6 @@ internal class DslModuleRepository(private val dsl: ArciphantDsl) : ModuleReposi
 
     override fun load() = dsl.modules.map { it.createFunctionalModule() } + dsl.bundles.map { it.createBundleModule() }
 
-    private fun BundleModuleDsl.createBundleModule(): BundleModule {
-        return BundleModule(
-            reference = if (name != null) ChildBundleModuleReference(name) else RootBundleModuleReference,
-            plugin = plugin,
-            includes = includes.ifEmpty { dsl.modules.map { it.reference } }
-        )
-    }
-
     private fun FunctionalModuleDsl.createFunctionalModule(): FunctionalModule {
         val stencil = build()
         val dependencies = stencil.dependencies.toDependencyMap()
@@ -33,5 +25,13 @@ internal class DslModuleRepository(private val dsl: ArciphantDsl) : ModuleReposi
     private fun List<ComponentDependency>.toDependencyMap() =
         groupBy(ComponentDependency::source) { Dependency(it.dependsOn, it.type) }
             .withDefault { emptyList() }
+
+    private fun BundleModuleDsl.createBundleModule(): BundleModule {
+        return BundleModule(
+            reference = if (name != null) ChildBundleModuleReference(name) else RootBundleModuleReference,
+            plugin = plugin,
+            includes = includes.ifEmpty { dsl.modules.map { it.reference } }
+        )
+    }
 
 }
