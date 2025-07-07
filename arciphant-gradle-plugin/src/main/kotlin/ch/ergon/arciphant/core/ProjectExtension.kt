f0ed4ecaa6c744db5d32fbf55334a74b203e4577
Christian Bosshard <bosshardchr@gmail.com>
2025-07-07 17:45:49 +0200

    Remove unused method.

--- arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/core/ProjectExtension.kt
+++
@@ -1,6 +0,0 @@
-package ch.ergon.arciphant.core
-
-import ch.ergon.arciphant.model.NamedReference
-import org.gradle.api.Project
-
-internal fun Project.childProject(reference: NamedReference) = childProjects.getValue(reference.name)
