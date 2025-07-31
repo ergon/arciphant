ba34c00a9cf8a35924979cb7cb4650c68fddde71
Christian Bosshard <bosshardchr@gmail.com>
2025-07-31 09:45:03 +0200

    Remove unused stuff.

--- arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/util/CollectionExtension.kt
+++
@@ -1,5 +0,0 @@
-package ch.ergon.arciphant.util
-
-fun <K, V> List<Map<K, V>>.merge(): Map<K, V> {
-    return if (isNotEmpty()) reduce { mergedMap, map -> mergedMap + map } else emptyMap()
-}
--- arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/util/StringExtension.kt
+++
@@ -1,3 +0,0 @@
-package ch.ergon.arciphant.util
-
-internal fun String?.emptyToNull() = this?.let { ifEmpty { null } }
