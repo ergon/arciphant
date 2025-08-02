3205ff42e72718430615b4c680681cedc7875d64
Christian Bosshard <bosshardchr@gmail.com>
2025-08-02 14:45:18 +0200

    Use logger instead of println.

--- arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/analyze/ProjectDependencies.kt
+++ arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/analyze/ProjectDependencies.kt
@@ -2,22 +2,23 @@ package ch.ergon.arciphant.analyze
 
 import org.gradle.api.Project
 import org.gradle.api.artifacts.ProjectDependency
+import org.gradle.api.logging.Logger
 
 internal fun Project.registerProjectDependenciesTask() {
     tasks.register("projectDependencies") {
         doLast {
-            printProjectDependencies()
+            printProjectDependencies(logger)
         }
     }
 }
 
-private fun Project.printProjectDependencies() {
+private fun Project.printProjectDependencies(logger: Logger) {
     allprojects.forEach { currentProject ->
-        println("Project '${currentProject.path}'")
+        logger.quiet("Project '${currentProject.path}'")
         currentProject.configurations.forEach { configuration ->
             configuration.dependencies.withType(ProjectDependency::class.java).forEach { dependency ->
                 if (dependency.path != currentProject.path) {
-                    println(" |- [${configuration.name}] ${dependency.path}")
+                    logger.quiet(" |- [${configuration.name}] ${dependency.path}")
                 }
             }
         }
