4cc2185a90edeae894673f88f5d5cb65c83f5b52
Christian Bosshard <bosshardchr@gmail.com>
2025-11-27 13:43:15 +0100

    Implement additional check.

--- arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/core/GradleProjectPath.kt
+++ arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/core/GradleProjectPath.kt
@@ -2,6 +2,11 @@ package ch.ergon.arciphant.core
 
 internal data class GradleProjectPath private constructor(val projectNames: List<String>) {
 
+    init {
+        require(projectNames.none { it.isEmpty() }) {
+                "Cannot create ${GradleProjectPath::class.simpleName} with empty project names: $projectNames" }
+    }
+
     val value by lazy { projectNames.joinToString(separator = ":", prefix = ":") }
 
     companion object {
--- arciphant-gradle-plugin/src/test/kotlin/ch/ergon/arciphant/core/GradleProjectPathTest.kt
+++ arciphant-gradle-plugin/src/test/kotlin/ch/ergon/arciphant/core/GradleProjectPathTest.kt
@@ -4,6 +4,7 @@ import ch.ergon.arciphant.util.dynamicTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.assertThrows
 
 class GradleProjectPathTest {
 
@@ -20,6 +21,17 @@ class GradleProjectPathTest {
         val projectPath = GradleProjectPath.of("a", "", "b")
 
         assertThat(projectPath.value).isEqualTo(":a:b")
+    }
+
+    @Test
+    fun `it should not allow instantiating a path with empty project names`() {
+
+        val exception = assertThrows<IllegalArgumentException> {
+            // copy method is used as a workaround to invoke private constructor directly without factory method (since factory method would filter out empty names)
+            GradleProjectPath.of().copy(projectNames = listOf("a", "", "b"))
+        }
+
+        assertThat(exception.message).isEqualTo("Cannot create ${GradleProjectPath::class.simpleName} with empty project names: [a, , b]")
     }
 
     private fun GradleProjectPath.Companion.of(vararg projectNames: String) = GradleProjectPath.of(projectNames.toList())
