beb22450b5cce8aac2dc11a1242383a8e01f4800
Christian Bosshard <bosshardchr@gmail.com>
2026-08-25 19:35:15 +0200

    Extract methods.

--- arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/dsl/ArciphantProjectDsl.kt
+++ arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/dsl/ArciphantProjectDsl.kt
@@ -1,9 +1,11 @@
 package ch.ergon.arciphant.dsl
 
 import ch.ergon.arciphant.core.ComponentLayout
+import ch.ergon.arciphant.core.ComponentLayout.PROJECT
 import ch.ergon.arciphant.core.ComponentLayout.SOURCE_SET
 import ch.ergon.arciphant.core.SourceSetComponentSettings
 import ch.ergon.arciphant.core.gradleProjectPath
+import ch.ergon.arciphant.core.model.Component
 import ch.ergon.arciphant.core.model.ComponentReference
 import ch.ergon.arciphant.core.model.DependencyType
 import ch.ergon.arciphant.core.model.DependencyType.API
@@ -22,6 +24,13 @@ open class ArciphantProjectDsl internal constructor(
     private val componentLayout: ComponentLayout,
     private val componentSettings: SourceSetComponentSettings
 ) {
+    fun api(module: String, component: String) = dependency(API, module, component)
+
+    fun implementation(module: String, component: String) = dependency(IMPLEMENTATION, module, component)
+
+    private fun dependency(type: DependencyType, module: String, component: String) = requireComponentLayout(PROJECT) {
+        // TODO implement
+    }
 
     fun component(name: String): ComponentReference = requireComponentLayout(SOURCE_SET) {
         ComponentReference(name)
@@ -35,10 +44,8 @@ open class ArciphantProjectDsl internal constructor(
 
     private fun ComponentReference.dependency(type: DependencyType, module: String, component: String) =
         requireComponentLayout(SOURCE_SET) {
-            val targetModule = modules.filterIsInstance<FunctionalModule>().singleOrNull { it.reference.name == module }
-            verify(targetModule != null) { "Module with name '$module' does not exist." }
-            val targetComponent = targetModule.components.singleOrNull { it.reference.name == component }
-            verify(targetComponent != null) { "Component with name '$component' does not exist in module '$module'." }
+            val targetModule = getModule(module)
+            val targetComponent = targetModule.getComponent(component)
             val sourceSet = project.sourceSets().findByName(name)
             verify(sourceSet != null) { "Component with name '$name' does not exist in project '${project.path}'." }
 
@@ -52,6 +59,18 @@ open class ArciphantProjectDsl internal constructor(
                 )
             }
         }
+
+    private fun getModule(moduleName: String): FunctionalModule {
+        val module = modules.filterIsInstance<FunctionalModule>().singleOrNull { it.reference.name == moduleName }
+        verify(module != null) { "Module with name '$moduleName' does not exist." }
+        return module
+    }
+
+    private fun FunctionalModule.getComponent(componentName: String): Component {
+        val component = components.singleOrNull { it.reference.name == componentName }
+        verify(component != null) { "Component with name '$componentName' does not exist in module '${this.reference.name}'." }
+        return component
+    }
 
     private fun <R> requireComponentLayout(componentLayout: ComponentLayout, block: () -> R): R {
         arciphantPrecondition(componentLayout == this.componentLayout) {
