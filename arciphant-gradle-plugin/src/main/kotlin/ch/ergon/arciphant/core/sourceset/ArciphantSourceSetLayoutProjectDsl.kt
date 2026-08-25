1370f2104aebbc4176a2ecf92031fba0932f9aab
Christian Bosshard <bosshardchr@gmail.com>
2026-08-25 20:23:52 +0200

    Split DSL by component layout.

---
+++ arciphant-gradle-plugin/src/main/kotlin/ch/ergon/arciphant/core/sourceset/ArciphantSourceSetLayoutProjectDsl.kt
@@ -0,0 +1,47 @@
+package ch.ergon.arciphant.core.sourceset
+
+import ch.ergon.arciphant.core.SourceSetComponentSettings
+import ch.ergon.arciphant.core.gradleProjectPath
+import ch.ergon.arciphant.core.model.Component
+import ch.ergon.arciphant.core.model.ComponentReference
+import ch.ergon.arciphant.core.model.DependencyType
+import ch.ergon.arciphant.core.model.DependencyType.API
+import ch.ergon.arciphant.core.model.DependencyType.IMPLEMENTATION
+import ch.ergon.arciphant.core.model.FunctionalModule
+import ch.ergon.arciphant.core.model.Module
+import ch.ergon.arciphant.core.model.getByName
+import ch.ergon.arciphant.core.model.getComponent
+import ch.ergon.arciphant.util.verify
+import org.gradle.api.Project
+
+open class ArciphantSourceSetLayoutProjectDsl internal constructor(
+    private val project: Project,
+    private val modules: List<Module>,
+    private val componentSettings: SourceSetComponentSettings
+) {
+
+    fun component(name: String) = ComponentReference(name)
+
+    fun ComponentReference.api(module: String, component: String) =
+        dependency(API, module, component)
+
+    fun ComponentReference.implementation(module: String, component: String) =
+        dependency(IMPLEMENTATION, module, component)
+
+    private fun ComponentReference.dependency(type: DependencyType, module: String, component: String) {
+            val targetModule = modules.getByName(module)
+            val targetComponent = targetModule.getComponent(component)
+            val sourceSet = project.sourceSets().findByName(name)
+            verify(sourceSet != null) { "Component with name '$name' does not exist in project '${project.path}'." }
+
+            project.sourceSetDependencies(componentSettings) {
+                addProjectDependency(
+                    type = type,
+                    sourceSet = sourceSet,
+                    projectPath = targetModule.gradleProjectPath().value,
+                    componentName = targetComponent.reference.name,
+                    withTestFixturesSourceSet = targetComponent.withTestFixturesSourceSet,
+                )
+            }
+        }
+}
