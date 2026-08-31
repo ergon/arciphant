f41e34af737d5cdcdc8128f6a35be6d71fe94cee
Christian Bosshard <bosshardchr@gmail.com>
2026-08-31 11:30:03 +0200

    Apply arciphant plugin in precompiled script plugin.

--- arciphant-source-set-demo/build-logic/settings.gradle.kts
+++ arciphant-source-set-demo/build-logic/settings.gradle.kts
@@ -1,3 +1,6 @@
+// makes the arciphant plugin available on the compile classpath of the precompiled script plugins
+includeBuild("../../arciphant-gradle-plugin")
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
