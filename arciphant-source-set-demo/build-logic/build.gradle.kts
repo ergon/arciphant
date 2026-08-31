930fad14928c2197d0e742b58f9376e974290de2
Christian Bosshard <bosshardchr@gmail.com>
2026-08-31 11:30:03 +0200

    Apply arciphant plugin in precompiled script plugin.

--- arciphant-source-set-demo/build-logic/build.gradle.kts
+++ arciphant-source-set-demo/build-logic/build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
 dependencies {
     // required since Kotlin plugin is applied from within the module-plugin
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
+    // resolved from the included build 'arciphant-gradle-plugin' (dependency substitution)
+    implementation("ch.ergon.arciphant:arciphant-gradle-plugin")
 }
