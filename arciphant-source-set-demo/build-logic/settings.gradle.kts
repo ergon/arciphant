82045d5082bd261f70f9825667a4019170e013b8
Christian Bosshard <bosshardchr@gmail.com>
2026-08-31 15:45:22 +0200

    Revert "Apply arciphant plugin in precompiled script plugin."
    This reverts commit 930fad14928c2197d0e742b58f9376e974290de2.

--- arciphant-source-set-demo/build-logic/settings.gradle.kts
+++ arciphant-source-set-demo/build-logic/settings.gradle.kts
@@ -1,6 +1,3 @@
-// makes the arciphant plugin available on the compile classpath of the precompiled script plugins
-includeBuild("../../arciphant-gradle-plugin")
-
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
